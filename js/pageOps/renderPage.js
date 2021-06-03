8ce4d61cf3bb66025f5f880f4eb78e6b631aab20
Simmarith <menzeladrian@googlemail.com>
2021-06-03 20:51:45 +0200

    jsdoc for PageStore also updated PageContent to proper naming

--- js/pageOps/renderPage.js
+++ js/pageOps/renderPage.js
@@ -1,49 +1,49 @@
 function renderPage(pageContent) {
-  if (pageContent.line.length > 24) {
-    pageContent.line.splice(24, pageContent.line.length - 24)
+  if (pageContent.lines.length > 24) {
+    pageContent.lines.splice(24, pageContent.lines.length - 24)
     console.log("cutting excess lines")
   }
-  while (pageContent.line.length > 24) {
-    pageContent.line.pop();
+  while (pageContent.lines.length > 24) {
+    pageContent.lines.pop();
     console.log("too many lines");
 
   }
-  while (pageContent.line.length < 24) {
-    pageContent.line.push("²1³8");
+  while (pageContent.lines.length < 24) {
+    pageContent.lines.push("²1³8");
     console.log("needs more lines");
   }
   for (let i=0;i<24;i++) {
     let fontColor = 7;
     let fontBackground = 8;
     let lineChanged=false;
-    const fgcount = (pageContent.line[i].split("²").length -1) * 2;
-    const bgcount = (pageContent.line[i].split("³").length -1) * 2;
-    const charcount = pageContent.line[i].length - fgcount - bgcount;
+    const fgcount = (pageContent.lines[i].split("²").length -1) * 2;
+    const bgcount = (pageContent.lines[i].split("³").length -1) * 2;
+    const charcount = pageContent.lines[i].length - fgcount - bgcount;
     //console.log(charcount);
     if (charcount > 40) {
       console.log("line",i,"too long");
-      pageContent.line[i] = pageContent.line[i].substr(0,pageContent.line[i].length - (charcount - 40));
+      pageContent.lines[i] = pageContent.lines[i].substr(0,pageContent.lines[i].length - (charcount - 40));
     }
     if (charcount < 40) {
       console.log("line",i,"too short");
       for (let j=charcount;j<40;j++) {
-        pageContent.line[i] += " ";
+        pageContent.lines[i] += " ";
       }
     }
-    for (let j=0;j<pageContent.line[i].length;j++) {
+    for (let j=0;j<pageContent.lines[i].length;j++) {
       let changed = false;
-      if (pageContent.line[i].charAt(j) == "²") {
+      if (pageContent.lines[i].charAt(j) == "²") {
         changed=true;
         lineChanged=true;
 
-        fontColor = pageContent.line[i].charAt(j+1);
-        pageContent.line[i] = pageContent.line[i].substr(0,j) + pageContent.line[i].substr(j+2);
+        fontColor = pageContent.lines[i].charAt(j+1);
+        pageContent.lines[i] = pageContent.lines[i].substr(0,j) + pageContent.lines[i].substr(j+2);
       }
-      if (pageContent.line[i].charAt(j) == "³") {
+      if (pageContent.lines[i].charAt(j) == "³") {
         changed=true;
         lineChanged=true;
-        fontBackground = pageContent.line[i].charAt(j+1);
-        pageContent.line[i] = pageContent.line[i].substr(0,j) + pageContent.line[i].substr(j+2);
+        fontBackground = pageContent.lines[i].charAt(j+1);
+        pageContent.lines[i] = pageContent.lines[i].substr(0,j) + pageContent.lines[i].substr(j+2);
       }
       if (changed==true) {
         let fontString = "";
@@ -51,24 +51,24 @@ function renderPage(pageContent) {
           fontString += "</span>"
         }
         fontString += "<span class=\"f" + fontColor + " b" + fontBackground + "\">";
-        pageContent.line[i] = pageContent.line[i].substr(0,j) + fontString + pageContent.line[i].substr(j);
+        pageContent.lines[i] = pageContent.lines[i].substr(0,j) + fontString + pageContent.lines[i].substr(j);
       }
     }
     if (lineChanged) {
-      pageContent.line[i] += "</span>";
+      pageContent.lines[i] += "</span>";
     }
     //const regex = '\\d{3}';
     const regex = '[1-8][0-9][0-9]';
-    const result = pageContent.line[i].match(regex);
+    const result = pageContent.lines[i].match(regex);
     if (result != null) {
-      pageContent.line[i] = pageContent.line[i].substr(0,result.index) + "<span class=\"link\" onclick=\"PageManager.page='" + pageContent.line[i].substr(result.index,3) + "'\">" + pageContent.line[i].substr(result.index,3) + "</span>" + pageContent.line[i].substr(result.index+3);
+      pageContent.lines[i] = pageContent.lines[i].substr(0,result.index) + "<span class=\"link\" onclick=\"PageManager.page='" + pageContent.lines[i].substr(result.index,3) + "'\">" + pageContent.lines[i].substr(result.index,3) + "</span>" + pageContent.lines[i].substr(result.index+3);
       //console.log(i,pageContent.line[i]);
     }
 
     //pageContent.line[i] = "12345<span class=\"f" + (Math.floor(Math.random() * 8) + 1) + " b"+(Math.floor(Math.random() * 8) + 1)+"\">" + page + "</span>90123456789012345678901234567890";
 
   }
-  let output = pageContent.line.join("<br />");
+  let output = pageContent.lines.join("<br />");
 
   document.getElementById("content").innerHTML = output;
 }
--- js/pageStore.js
+++ js/pageStore.js
@@ -1,27 +1,33 @@
-/*
- * TODO: proper JSDoc
- *
- * this store incorporates:
+/**
+ *  @typedef {Object} PageContent
+ *  @property {string} title
+ *  @property {Array} lines
+ */
+
+/**
+ * The preferred way to get Pages from the backend. This also implements:
  * - fetching data from the server
  * - chaching in a variable
  * - invalidating pages after cacheTime
  * - flushing cache
  * - disabling cache
- *
- * Cache structure:
- * {
- *  <pageNumber>: {
- *    // if invalid date, keep forever
- *    validUntil: <number>,
- *    pageContent: <JSON>
- *  }
- * }
- **/
-
-const pageStore = {
-  cacheTime: 10000,
+ * 
+ * @property {number} cacheTime time a page stays valid in cache in ms - defaults to 100s
+ * @property {boolean} keepCache disregard cache alltogether if false - defaults to true
+ * @property {Array} pages the actual cache in memory
+ */
+const PageStore = {
+  // 100 seconds
+  cacheTime: 100000,
   keepCache: true,
   pages: {},
+  /**
+   * get a PageContent either from cache or from the backend
+   *
+   * @param {number} pageNumber
+   *
+   * @returns {PageContent} page
+   */
   async get(pageNumber) {
     // TODO: Persistence
     if (!this.keepCache ||
@@ -34,15 +40,35 @@ const pageStore = {
     const cachedPageContent = this.pages[pageNumber].pageContent
     return {
       title: cachedPageContent.title,
-      line: [...cachedPageContent.line]
+      lines: [...cachedPageContent.lines]
     }
   },
+
+  /**
+   * set a page in cache - not implemented jet
+   *
+   * @param {number} pageNumber
+   * @param {PageContent} pageContent
+   */
   set(pageNumber, pageContent) { 
 
   },
+
+  /**
+   * flush the entire page cache
+   */
   flush() {
     this.pages = {}
   },
+
+  /**
+   * update one page in cache from the backed - automatically resets validity based on
+   * this.cacheTime
+   *
+   * @param {number} pageNumber
+   *
+   * @returns {Promise<PageContent>} pageContent
+   */
   update(pageNumber) {
     return this.fetch(pageNumber)
       .then(pageContent => {
@@ -50,17 +76,26 @@ const pageStore = {
           pageContent,
           validUntil: new Date().getTime() + this.cacheTime
         }
+        return pageContent
       })
   },
+
+  /**
+   * fetch diretly from the backend
+   *
+   * @param {number} pageNumber
+   *
+   * @returns {Promise<PageContent>} pageContent
+   */
   fetch(pageNumber) {
     let returnObj = {};
     let random = Math.floor(Math.random()*50000)
     const requestURL = "content/jvpeek/" + pageNumber + ".json?r=" + random
-    returnObj.line = []
+    returnObj.lines = []
 
     return fetch(requestURL)
       .then(res => res.json())
   }
 }
 
-export default pageStore
+export default PageStore
